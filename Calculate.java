5fa557d6b73098f0e36b9413164dfcaac581bfb1
AlexBary2020 <abaryshn10@yandex.ru>
2021-07-01 22:53:20 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,18 @@
-public class Calculate{
-	public static void main(String[] args){
-	System.out.println("Calculate...");
-	}
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Calculate...");
+        int first = Integer.valueOf(args[0]);
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int sum = first + second;
+        int sub = first - second;
+        int mul=first*second;
+        int div=first/second;
+        System.out.println("Sum is "+sum);
+        System.out.println("Subtract is "+sub);
+        System.out.println("Multiply is "+mul);
+        System.out.println("Division is "+div);
+
+    }
 
 }
